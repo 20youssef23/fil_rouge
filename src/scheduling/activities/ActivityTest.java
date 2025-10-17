00060cbdab5b0e097e23ffd8a5c547918c200774
Youssef Chakri <chakri251@CAMPUS@V304V-NCP029.campus.unicaen.fr>
2025-10-17 13:33:29 +0200

    avant derier version

--- src/scheduling/activities/ActivityTest.java
+++ src/scheduling/activities/ActivityTest.java
@@ -1,13 +1,25 @@
 package scheduling.activities;
 
-import schedulingtests.activities.ActivityTests;
+import schedulingtests.activities.ActivityTests; // Import du fichier de tests fourni dans le .jar
 
 public class ActivityTest {
+
     public static void main(String[] args) {
+
+        testActivityManual("Aller à l'université", 15);
+        testActivityManual("Attacher le vélo", 1);
+
         boolean ok = true;
         ActivityTests activityTester = new ActivityTests();
         ok = ok && activityTester.testGetDescription();
         ok = ok && activityTester.testGetDuration();
         System.out.println(ok ? "All tests passed" : "At least one test failed");
+    }
+
+
+    public static void testActivityManual(String description, int duration) {
+        Activity activity = new Activity(description, duration);
+        System.out.println("Description : " + activity.getDescription());
+        System.out.println("Durée : " + activity.getDuration() + " minutes");
     }
 }
--- src/scheduling/basictopologicalsort/TopologicalSorterTest.java
+++ src/scheduling/basictopologicalsort/TopologicalSorterTest.java
@@ -10,7 +10,41 @@ public class TopologicalSorterTest {
     public static void main(String[] args) {
         TopologicalSorter topologicalSorter = new TopologicalSorter();
 
-        Activity getup = new Activity("get up", 1);
+        Activity getUp = new Activity("get up", 1);
+        Activity goToWork = new Activity("go to work", 15);
+        Activity shower = new Activity("take a shower", 10);
+        Activity brushTeeth = new Activity("brush teeth", 3);
+        Activity getDressed = new Activity("get dressed", 2);
+        Activity breakfast = new Activity("have breakfast", 15);
+
+
+        HashSet<Activity> activities = new HashSet<>();
+        activities.add(getUp);
+        activities.add(goToWork);
+        activities.add(shower);
+        activities.add (brushTeeth);
+        activities.add(getDressed);
+        activities.add(breakfast);
+
+        HashSet<PrecedenceConstraint> constraints = new HashSet<>();
+        constraints.add(new PrecedenceConstraint(getUp,breakfast));
+        constraints.add(new PrecedenceConstraint(getUp, getDressed));
+        constraints.add(new PrecedenceConstraint(breakfast, brushTeeth));
+        constraints.add(new PrecedenceConstraint(shower, getDressed));
+        constraints.add(new PrecedenceConstraint(brushTeeth, goToWork));
+        constraints.add(new PrecedenceConstraint(getDressed, goToWork));
+        constraints.add(new PrecedenceConstraint(getUp, shower));
+        constraints.add(new PrecedenceConstraint(breakfast, goToWork));
+
+        ArrayList<Activity> order = topologicalSorter.bruteForceSort(activities,constraints);
+        HashMap<wActivity, Integer> schedule = topologicalSorter.schedule(activities,constraints,500);
+
+        System.out.println("order et horaires");
+        for (activities a : order){
+            System.out.println(" " + a.getDescription() + ":" + schedule.get(a));
+        }
+
+
 
     }
 }
